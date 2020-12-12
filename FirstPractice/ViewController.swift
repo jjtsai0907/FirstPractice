9aafe00111d7ce101888ea6200db1e25ca284b8e
jia.jiuan.tsai <jia.jiuan.tsai@iths.se>
2020-12-12 11:32:55 +0100

    Initial Commit

---
+++ FirstPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstPractice
+//
+//  Created by Jia-Jiuan Tsai on 2020-12-12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
